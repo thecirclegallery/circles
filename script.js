14f8960c76b445eb6e7d007b5c14501c9854277b
thecirclegallery <lopsthemonki@gmail.com>
2026-05-09 20:02:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 // Disable Right-Click
 document.addEventListener('contextmenu', (e) => e.preventDefault());
 
-// Setup the System Variables
 const icons = document.querySelectorAll('.file-icon');
 const imageWindow = document.getElementById('image-window');
 const windowImg = document.getElementById('window-img');
 const closeBtn = document.getElementById('close-btn');
 
-// Make the icons clickable
 icons.forEach(icon => {
     icon.addEventListener('click', () => {
-        // Find which image this specific icon is holding
         const src = icon.getAttribute('data-src');
-        
-        // Put that image into the main window
         windowImg.src = src;
-        
-        // Open the window
         imageWindow.classList.remove('hidden');
     });
 });
 
-// Make the Close Button work
 closeBtn.addEventListener('click', () => {
     imageWindow.classList.add('hidden');
 });
